5782955983f73f6c2cfaa986c6eaaecac9eaf236
Vladislav <danirucakov@gmail.com>
2024-08-31 10:49:21 +0300

    changed blurSVG

--- src/config/svg.ts
+++ src/config/svg.ts
@@ -1,13 +1,19 @@
 export const svg = {
-  blurSVG: (base64: string) =>
-    `
-		<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="100%">
-			<defs>
-				<filter id="blur-filter">
-					<feGaussianBlur stdDeviation="10" />
-				</filter>
-			</defs>
-			<image href="data:image/avif;base64,${base64}" width="100%" height="100%" filter="url(#blur-filter)" />
+  blurSVG: (base64: string) => `
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox='0 0 8 5'>
+			<filter id='b' color-interpolation-filters='sRGB'>
+				<feGaussianBlur stdDeviation='1' />
+			</filter>
+
+			<image 
+				preserveAspectRatio='none' 
+				filter='url(#b)' 
+				x='0' 
+				y='0' 
+				height='100%' 
+				width='100%' 
+				href='data:image/avif;base64,${base64}' 
+			/>
 		</svg>
 	`,
 }
